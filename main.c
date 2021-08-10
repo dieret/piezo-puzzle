1274fd8472494ae0eab0885820f2362d165135be
Kilian Lieret <kilian.lieret@posteo.de>
2021-08-10 21:18:26 +0200

    clang-format

--- main.c
+++ main.c
@@ -4,14 +4,13 @@
 #define __DELAY_BACKWARD_COMPATIBLE__
 #include <util/delay.h>
 
-
-#define PORT_(port) PORT ## port
-#define DDR_(port)  DDR  ## port
-#define PIN_(port)  PIN  ## port
+#define PORT_(port) PORT##port
+#define DDR_(port) DDR##port
+#define PIN_(port) PIN##port
 
 #define PORT(port) PORT_(port)
-#define DDR(port)  DDR_(port)
-#define PIN(port)  PIN_(port)
+#define DDR(port) DDR_(port)
+#define PIN(port) PIN_(port)
 
 // FIXME
 #define SPEAKER_PORT D
@@ -21,29 +20,23 @@
 
 void beep(float freq, float duration);
 
-void initialize_ports(void) {
-    DDR(SPEAKER_PORT) |= (1 << SPEAKER_PIN);
-}
+void initialize_ports(void) { DDR(SPEAKER_PORT) |= (1 << SPEAKER_PIN); }
 
-int main(void) {
-    initialize_ports();
-}
+int main(void) { initialize_ports(); }
 
 // Logic from https://www.petervis.com/C/pizo%20speaker/pizo%20speaker.html
-void beep(float freq, float duration)
-{
+void beep(float freq, float duration) {
 
-    float wavelength=(1/freq)*1000;
-    uint32_t cycles=duration/wavelength;
-    float half_period = wavelength/2;
+  float wavelength = (1 / freq) * 1000;
+  uint32_t cycles = duration / wavelength;
+  float half_period = wavelength / 2;
 
-    for (uint32_t i=0; i<cycles; i++)
-    {
-        _delay_ms(half_period);
-        PORT(SPEAKER_PORT) |= (1 << SPEAKER_PIN);
-        _delay_ms(half_period);
-        PORT(SPEAKER_PORT) &= ~(1 << SPEAKER_PIN);
-    }
+  for (uint32_t i = 0; i < cycles; i++) {
+    _delay_ms(half_period);
+    PORT(SPEAKER_PORT) |= (1 << SPEAKER_PIN);
+    _delay_ms(half_period);
+    PORT(SPEAKER_PORT) &= ~(1 << SPEAKER_PIN);
+  }
 
-    return;
+  return;
 }
